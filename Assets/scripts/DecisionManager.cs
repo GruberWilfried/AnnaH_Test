6561b7ccdd1336015ef199a0c3dc7a149be302b8
Will <wilfried.gruber@hotmail.com>
2020-03-31 15:18:26 +0200

    Imported Annas Project

---
+++ Assets/scripts/DecisionManager.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class DecisionManager : MonoBehaviour
+{
+
+    public string[] videoclips;
+    public VideoPlayer[] videoPlayer;
+    public int number = 0;
+    public float time;
+    public float[] maxTime;
+    public bool timeRunning;
+    public GameObject[] EntscheidungsButtons;
+
+    // Use this for initialization
+    void Start()
+    {
+        //wenn Ende des Videos erreicht ist wird Funktion PlayVideos aufgerufen
+        //videoPlayer.loopPointReached += NextVideo;
+        for (int i = 0; i < videoPlayer.Length; i++)
+        {
+            videoPlayer[i].loopPointReached += NextVideoPlayer;
+        }
+        
+    }
+
+    public void NextVideoPlayer(VideoPlayer vid)
+    {
+        videoPlayer[number].gameObject.GetComponent<MeshRenderer>().enabled = false;
+        if (number < (videoPlayer.Length - 1))
+        {
+            videoPlayer[number + 1].gameObject.GetComponent<MeshRenderer>().enabled = true;
+            //videoPlayer[number + 1].clip = videoclips[number + 1];
+            videoPlayer[number + 1].Play();
+
+            EntscheidungsButtons[number].SetActive(false); // Buttons werden ausgeblendet
+            number = number + 1;
+            timeRunning = true;
+        }
+
+    }
+    /*
+    public void NextVideo(VideoPlayer vid)
+    {
+        //Wenn Video Nummer unter 3 ist wird immer wieder das Nummer darauffolgende Video abgespiet 
+        if (number < 3)
+        {
+            videoPlayer.clip = videoclips[number];
+            videoPlayer.Play();
+            EntscheidungsButtons[number].SetActive(false); // Buttons werden ausgeblendet
+            number = number + 1;
+            timeRunning = true;
+        }
+        //Wenn es drüber ist dann wird nicht mehr dazugerechnet 
+        else
+        {
+            //zeigt das wir keine Videos mehr haben, Ende ist erreicht 
+            Debug.Log("End Reached");
+        }
+    }
+    */
+
+    public void SetTimerActive()
+    {
+        timeRunning = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //wenn der Start button gedrückt wird bzw set active is true dann startet der Timer zu zählen
+        if (timeRunning && number < maxTime.Length)
+        {
+            //wenn die Zeit noch größer 0 ist wird Zeit abgezogen
+            if (maxTime[number] > 0)
+            {
+                maxTime[number] -= Time.deltaTime;
+            }
+            //zeit ist abgelaufen 
+            else
+            {
+                Debug.Log("Time is up");
+                EntscheidungsButtons[number].SetActive(true);
+                timeRunning = false;
+                videoPlayer[number + 1].url = videoclips[number + 1];
+                videoPlayer[number + 1].Prepare();
+
+            }
+        }
+
+    }
+
+    public void NewVideo(string url)
+    {
+        videoclips[number + 1] = url;
+        videoPlayer[number + 1].url = videoclips[number + 1];
+        videoPlayer[number + 1].Prepare();
+    }
+
+    public void NewMaxTimeNextVideo(float time)
+    {
+        maxTime[number] = time;
+    }
+}
---
+++ Assets/scripts/Entscheidung.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Entscheidung : MonoBehaviour
+{
+
+    public VideoPlayer vid;
+    public VideoPlayer entsch1Vid;
+    public string url;
+    public string entsch1Url;
+
+    public GameObject startButton;
+    public GameObject Entscheidung1;
+    //public GameObject chooseRandom;
+    public float timeWaited;
+    public float time;
+    public float maxTime = 5f;
+    public float timeEntscheidung;
+    public float maxTimeEntscheidung = 10f;
+
+    public GameObject videoAntworten;
+    public GameObject videoNichtAntworten;
+    public string videoAntwortenURL;
+    public string videoNichtAntwortenURL;
+
+    //public float EndReached;
+
+    public void Start()
+    {
+        vid.prepareCompleted += StartVideo;
+        vid.prepareCompleted += StartEntsch1Video;
+        time = maxTime;
+        timeEntscheidung = maxTimeEntscheidung;
+        //        vid.loopPointReached += EndReached;
+    }
+
+    public void SetUrl()
+    {
+        vid.url = url;
+        vid.Prepare();
+    }
+    public void SetEntschUrl()
+    {
+        entsch1Vid.url = entsch1Url;
+        vid.Prepare();
+    }
+    public void StartVideo(VideoPlayer vp)
+    {
+        Debug.Log("Video is played");
+        vp.Play();
+        startButton.SetActive(false);
+    }
+
+    public void StartEntsch1Video(VideoPlayer vp)
+    {
+        Debug.Log("Entsch1 is played");
+        vp.Play();
+        //videoAntworten.SetActive(false);
+    }
+
+    public void EndReached()
+    {
+        if (videoAntworten == true)
+        {
+            //play.videoAntwortenURL;
+        }
+        else if (videoNichtAntworten == true)
+        {
+
+        }
+    }
+
+    public void Update()
+    {
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+        }
+        else if (time <= 0)
+        {
+            Entscheidung1.SetActive(true);
+
+            if (timeEntscheidung > 0)
+            {
+                timeEntscheidung -= Time.deltaTime;
+            }
+            else if (timeEntscheidung <= 0)
+            {
+                //chooseRandom.SetActive(false);
+
+            }
+        }
+
+    }
+}
